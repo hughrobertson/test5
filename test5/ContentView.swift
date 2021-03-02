b59c1865023a9b68ad468e1d49dd91f3cd8c1d38
hughrobertson <hughrobertson44@gmail.com>
2021-03-02 17:59:45 +0000

    Initial Commit

---
+++ test5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test5
+//
+//  Created by hugh robertson on 02/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test5/test5App.swift
@@ -0,0 +1,17 @@
+//
+//  test5App.swift
+//  test5
+//
+//  Created by hugh robertson on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct test5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
